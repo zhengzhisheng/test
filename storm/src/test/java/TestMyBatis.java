8eb45db3d013d5e7380c730873e2fc7527776f75
zzs7758 <23217286334@qq.com>
2016-08-25 23:35:33 +0800

    Signed-off-by: zzs7758 <23217286334@qq.com>

---
+++ storm/src/test/java/TestMyBatis.java
@@ -0,0 +1,40 @@
+
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.alibaba.fastjson.JSON;
+import com.cn.hnust.pojo.User;
+import com.cn.hnust.service.IUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner�?
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
+
+public class TestMyBatis {
+	private static Logger logger = Logger.getLogger(TestMyBatis.class);
+//	private ApplicationContext ac = null;
+	@Resource
+	private IUserService userService = null;
+
+//	@Before
+//	public void before() {
+//		ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+//		userService = (IUserService) ac.getBean("userService");
+//	}
+
+	@Test
+	public void test1() {
+		User user = userService.getUserById(1);
+		// System.out.println(user.getUserName());
+		// logger.info("值："+user.getUserName());
+		logger.info(JSON.toJSONString(user));
+	}
+}
